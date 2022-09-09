12915dcd575efb73a5c5c86a1eccdd3d13888cca
Vitor Marcato <vitor.marcatof@gmail.com>
2022-09-09 13:51:03 -0300

    add spring files

---
+++ loginform/loginform/src/main/java/br/com/personalproject/loginform/teste.java
@@ -0,0 +1,5 @@
+package br.com.personalproject.loginform;
+
+public class teste {
+
+}
---
+++ loginform/loginform/src/test/java/br/com/personalproject/loginform/LoginformApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.personalproject.loginform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
